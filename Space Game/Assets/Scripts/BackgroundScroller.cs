dc289cb65752d9eda443a9e089fe0fb20521f561
cppcrusader <burns.s@gmail.com>
2024-11-12 15:49:45 -0500

    Updated BackgroundScroller to reset obstacles

--- Space Game/Assets/Scripts/BackgroundScroller.cs
+++ Space Game/Assets/Scripts/BackgroundScroller.cs
@@ -38,6 +38,7 @@ public class BackgroundScroller : MonoBehaviour
 
         //set the velocity to a vector using our scrollSpeed and scrollDirection
         rb.velocity = new Vector2((scrollDirection * scrollSpeed), 0);
+        ResetObstacle();
     }
 
     // Update is called once per frame
@@ -49,6 +50,12 @@ public class BackgroundScroller : MonoBehaviour
         {
             Vector2 resetPosition = new Vector2(width * 2f, 0);
             transform.position = (Vector2)transform.position + resetPosition;
+            ResetObstacle();
         }
+    }
+
+    void ResetObstacle()
+    {
+        transform.GetChild(0).localPosition = new Vector3(0, Random.Range(-3, 3), -1);
     }
 }
